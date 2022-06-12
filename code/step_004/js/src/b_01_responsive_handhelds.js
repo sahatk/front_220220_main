1e60c70ccad375f5e3fc5c723ed0c46d2961a732
sangjin Park <sahatk@naver.com>
2022-06-12 17:56:41 +0900

    2022-06-12

---
+++ code/step_004/js/src/b_01_responsive_handhelds.js
@@ -0,0 +1,43 @@
+// b_01_responsive_handhelds.js
+
+//1  .navi_btn 내부 button요소를 선택 시 -> 형제요소인 .navi_area를 .action의 add/remove 처리
+
+{  
+    // 변수 ---------------------------------------------------------------------------
+
+    // 기본 변수
+    const naviPart = document.querySelector(".navigation_part");
+    const naviH2 = naviPart.querySelector('h2');
+    const naviArea = document.querySelector(".navi_area");
+
+    // 기본 기능처리
+    const btnCode = `<button type="button"><i class="fa-solid fa-bars"></i><span>네비게이션 버튼</span></button>`;
+    const makeNaviBtn = document.createElement('div');
+    makeNaviBtn.setAttribute('class', 'navi_btn blind_area');
+    makeNaviBtn.innerHTML = btnCode;
+    naviH2.after(makeNaviBtn);
+  
+
+    // 추가 선택자
+    const naviBtn = document.querySelector(".navi_btn");
+    const naviBtnSel = [].slice.call(naviBtn.children)[0];
+    //children은 유사배열이기에 배열기능을 사용 할 수 없다. -> 진짜 배열로 변환하여 사용
+    const naviBtnI = naviBtn.querySelector("i");
+    const BTNICON = ["fa-bars", "fa-xmark"];
+    const naviDisplay = ['flex', 'none'];
+
+    // 이벤트 ------------------------------------------------------------
+  naviBtnSel.addEventListener("click", function (e) {
+    e.preventDefault();
+    const check = window.getComputedStyle(naviArea).display === "none";
+    if (check) {
+      naviArea.style.display = naviDisplay[0];
+      naviBtnI.classList.remove(BTNICON[0]);
+      naviBtnI.classList.add(BTNICON[1]);
+    } else {
+      naviArea.style.display = naviDisplay[1];
+      naviBtnI.classList.remove(BTNICON[1]);
+      naviBtnI.classList.add(BTNICON[0]);
+    }
+  });
+}
--- code/step_004/js/src/b_01_responsive_js.js
+++ code/step_004/js/src/b_01_responsive_js.js
@@ -1,28 +1,59 @@
 // b_01_responsive_js.js
 
-//1  .navi_btn 내부 button요소를 선택 시 -> 형제요소인 .navi_area를 .action의 add/remove 처리
+// @media all and (max-width:1439px){}
 
+/*
+const deviceWidth = $(window).outerWidth();
+const setWidth = 1440;
 
-const naviBtn = document.querySelector('.navi_btn');
-const naviArea = document.querySelector('.navi_area');
-const naviBtnSel = [].slice.call(naviBtn.children)[0]; 
-//children은 유사배열이기에 배열기능을 사용 할 수 없다. -> 진짜 배열로 변환하여 사용
-const naviBtnI = naviBtn.querySelector('i');
-const ACTION_TEXT = 'action';
-const BTNICON = ['fa-bars','fa-xmark'];
+$(window).on('resize', function(e){
+    const afterWidth = $(window).outerWidth();
 
-naviBtnSel.addEventListener('click', function(e){
-    e.preventDefault();
-    // naviArea.classList.add(ACTION_TEXT);
-    const check = naviArea.classList.contains(ACTION_TEXT);
-    if(!check){
-        naviArea.classList.add(ACTION_TEXT);
-        naviBtnI.classList.remove(BTNICON[0]);
-        naviBtnI.classList.add(BTNICON[1]);
-    }else{
-        naviArea.classList.remove(ACTION_TEXT);
-        naviBtnI.classList.remove(BTNICON[1]);
-        naviBtnI.classList.add(BTNICON[0]);
+    if(deviceWidth !== afterWidth){
+        if(setWidth <= afterWidth){
+            // desktop 기준
+            location.reload();
+        }else{
+            // tablet이하 기준
+            location.reload();
+        }
     }
-});
+})
 
+*/
+{
+  // 변수 ---------------------------------------------
+
+  const dataFile = { handhelds: "../js/src/b_01_responsive_handhelds.js" };
+  const body = document.querySelector("body");
+  const naviArea = document.querySelector(".navi_area");
+
+  const desktopWidth = 1440;
+  const widthCheck = window.matchMedia(`(max-width: ${desktopWidth - 1}px)`);
+  
+  let handHelds;
+
+  // function -------------------------------------------
+  const fnHendHeldsScript = function () {
+    const makeScript = document.createElement("script");
+    makeScript.setAttribute("src", dataFile.handhelds);
+    makeScript.classList.add("handHelds");
+    body.append(makeScript);
+    handHelds = document.querySelector(".handHelds");
+  };
+
+  const fnHandHeldsRemove = function () {
+    handHelds.remove();
+    naviArea.removeAttribute("style");
+    const naviBtn = document.querySelector(".navi_btn");
+    naviBtn.remove();
+  };
+
+  // event/기능 --------------------------------------------------------
+  if (widthCheck.matches) {fnHendHeldsScript(); }
+  widthCheck.addEventListener("change", (e) => {
+    e.matches ? fnHendHeldsScript() : fnHandHeldsRemove();
+  });
+}
+
+// phone(600-1) : table(1024-1) : laptop(1366-1)
