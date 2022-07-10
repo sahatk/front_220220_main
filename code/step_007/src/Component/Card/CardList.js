2810bf1ceec3827570924f7faecd25082dcbf2c8
sangjin Park <sahatk@naver.com>
2022-07-10 20:31:26 +0900

    2022-07-10

--- code/step_007/src/Component/Card/CardList.js
+++ code/step_007/src/Component/Card/CardList.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-function CardList({attr}) {  
+// JSX안의 이미지 경로는 public JSX바깥의 이미지 경로는 src에 해두는것이 정석
+function CardList({attr}) {
+  const style = {backgroundImage:`url(${process.env.PUBLIC_URL}/img/cardList/${attr.img})`};
   return (
     <li>
-      <a herf={attr.link}>
+      <a href={attr.link} style={style}>
         <dl>
           <dt>{attr.title}</dt>
           <dd>{attr.content}</dd>
