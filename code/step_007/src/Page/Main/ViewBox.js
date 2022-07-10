6b7edb4fc56e9f8cb75434a7c118598e43e27ce6
sangjin Park <sahatk@naver.com>
2022-07-10 17:53:11 +0900

    2022-07-10

---
+++ code/step_007/src/Page/Main/ViewBox.js
@@ -0,0 +1,54 @@
+import React,{useEffect, useState} from 'react';
+import './ViewBox.scss';
+import {v4 as uuidv4} from 'uuid';
+
+function ViewBox() {
+  const total = 4;
+  const [count, setCount] = useState(0);
+  const listData = [1,2,3,4,5]
+  useEffect( () => {
+    console.log(count);    
+  }, [count]);
+
+  const handlerNextClick = (e) =>{
+    e.preventDefault();
+    count >= total ? setCount(0) : setCount(count + 1);
+
+  }
+  const handlerPrevClick = (e) =>{
+    e.preventDefault();
+    count <= 0 ? setCount(total) : setCount(count - 1);
+  }
+
+
+  return (
+    <section id='viewBox'>
+      <h2 className='blind'>광고 영역(ViewBox)</h2>
+      <div className='buttons'>
+        <button type='button' className='next' onClick={handlerNextClick}><span className='blind'>next</span></button>
+        <button type='button' className='prev' onClick={handlerPrevClick}><span className='blind'>prev</span></button>
+      </div>
+      <div className='indicators'>
+        <ul className='blind_area'>
+          { 
+          listData.map((data,index) => 
+            <li key={uuidv4()} className={count === index ? 'action' : null}>
+              <a href='#' onClick={(e) => {e.preventDefault(); return setCount(index)} }><span>{data}번째 광고 요약 설명</span></a>
+            </li>)
+          }
+        </ul>
+        <p>
+        <span className='now'>{count + 1}</span>/
+          <span className='total'>{total + 1}</span>
+        </p>
+      </div>
+      <div className='slide_wrapper fade_area'>
+        <ul>
+          {listData.map((data,index)=><li key={uuidv4()} className={count === index ? 'action' : null}>광고{data}</li>)}
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+export default ViewBox
